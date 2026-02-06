c273973f9e0e6db54fbb7c1fd9ee813fa6d99cc3
SONALI-Nikki <sonalipriyardarshini@gmail.com>
2026-02-06 17:22:44 +0530

    initial commit

--- index.js
+++ index.js
@@ -15,8 +15,9 @@ app.use(
   cors({
     origin: [
       "http://localhost:5173",
-      "https://bug-tracker-frontend-umber.vercel.app/",
+      "https://bug-tracker-frontend-umber.vercel.app",
     ],
+    methods: ["GET", "POST", "PUT", "DELETE"],
     credentials: true,
   })
 );
